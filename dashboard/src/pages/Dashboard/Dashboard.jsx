cf85c149a083197fd54b9c662f8b0bbc355aa391
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-11 17:19:48 +0530

    dashboard: create portfolio summary

--- dashboard/src/pages/Dashboard/Dashboard.jsx
+++ dashboard/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,65 @@
+import { useEffect, useState } from "react";
+
+import { getHoldings } from "../../services/holdings.service";
+
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const [holdings, setHoldings] = useState([]);
+
+  useEffect(() => {
+    const fetchHoldings = async () => {
+      try {
+        const data = await getHoldings();
+
+        setHoldings(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchHoldings();
+  }, []);
+
+  const totalInvestment = holdings.reduce(
+    (acc, holding) =>
+      acc + holding.avg * holding.qty,
+    0
+  );
+
+  const currentValue = holdings.reduce(
+    (acc, holding) =>
+      acc + holding.price * holding.qty,
+    0
+  );
+
+  const profitLoss =
+    currentValue - totalInvestment;
+
+  return (
+    <div>
+      <h1>Dashboard Summary</h1>
+
+      <hr />
+
+      <h2>
+        Total Holdings: {holdings.length}
+      </h2>
+
+      <h2>
+        Total Investment:
+        ₹{totalInvestment.toFixed(2)}
+      </h2>
+
+      <h2>
+        Current Value:
+        ₹{currentValue.toFixed(2)}
+      </h2>
+
+      <h2>
+        Profit / Loss:
+        ₹{profitLoss.toFixed(2)}
+      </h2>
+    </div>
+  );
 }
 
 export default Dashboard;
