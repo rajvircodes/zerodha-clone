608b359d4bb3020523f3c55d4069878d749a560a
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-13 17:00:26 +0530

    backend: create register api

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import healthRoutes from "./routes/health.routes.js";
 import holdingRoutes from "./routes/holding.routes.js";
-import positionRoutes from './routes/position.routes.js'
-import orderRoutes from './routes/order.routes.js'
+import positionRoutes from "./routes/position.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use("/api/health", healthRoutes);
 app.use("/api/holdings", holdingRoutes);
-app.use('/api/positions', positionRoutes);
+app.use("/api/positions", positionRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+
+import User from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
