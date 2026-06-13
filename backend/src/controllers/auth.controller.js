a6210c57ca1a5c690a2062d91d2a1454dd38be86
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-13 19:09:21 +0530

    backend: create login api and jwt authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 import User from "../models/User.js";
 
@@ -35,6 +36,70 @@ export const registerUser = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: error.message,
+    });
+  }
+};
+
+export const loginUser = async (
+  req,
+  res
+) => {
+  try {
+    const {
+      email,
+      password,
+    } = req.body;
+
+    const user =
+      await User.findOne({
+        email,
+      });
+
+    if (!user) {
+      return res.status(400).json({
+        message:
+          "Invalid credentials",
+      });
+    }
+
+    const isMatch =
+      await bcrypt.compare(
+        password,
+        user.password
+      );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message:
+          "Invalid credentials",
+      });
+    }
+
+    const token =
+      jwt.sign(
+        {
+          userId: user._id,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "7d",
+        }
+      );
+
+    res.status(200).json({
+      message:
+        "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error.message,
     });
   }
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 
-import {
-  registerUser,
-} from "../controllers/auth.controller.js";
+import { registerUser, loginUser } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post(
-  "/register",
-  registerUser
-);
+router.post("/register", registerUser);
+router.post("/login",loginUser);
 
 export default router;
