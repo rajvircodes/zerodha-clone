d446b56c799353ddd1eedc7fcbca0a86c9d9d272
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-13 19:18:51 +0530

    backend: add auth middleware and protect routes

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
-
-import { getOrders, createOrder, deleteOrder, updateOrder } from "../controllers/order.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import {
+  getOrders,
+  createOrder,
+  deleteOrder,
+  updateOrder,
+} from "../controllers/order.controller.js";
 
 const router = Router();
 
-router.get("/", getOrders);
-router.post("/", createOrder);
-router.delete('/:id', deleteOrder);
-router.put('/:id', updateOrder)
+router.get(
+  "/",
+  authMiddleware,
+  getOrders
+);
+
+router.post(
+  "/",
+  authMiddleware,
+  createOrder
+);
+
+router.put(
+  "/:id",
+  authMiddleware,
+  updateOrder
+);
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  deleteOrder
+);
 
 export default router;
