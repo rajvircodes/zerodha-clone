5ec43057d1f4e02fe00130ed8fe9135d5d96253a
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-14 22:59:41 +0530

    connect dashboard to production backend

--- dashboard/src/services/api.js
+++ dashboard/src/services/api.js
@@ -1,24 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://zerodha-clone-wrgr.onrender.com/api",
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token =
-      localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
